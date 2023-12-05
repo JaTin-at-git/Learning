5f488de00047685aabbd011bfc8146c5f9a63ca4
JaTin <jigyashusaini7@gmail.com>
2023-12-05 22:47:11 +0530

    adding tilingWays function to RectusionQuestions

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import recursion.RecursionQuestions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(RecursionQuestions.isSorted(new int[]{1, 2, 3, 4, 8, 6}));
+        System.out.println(RecursionQuestions.tilingWays(8));
     }
 }
--- src/recursion/RecursionQuestions.java
+++ src/recursion/RecursionQuestions.java
@@ -8,8 +8,23 @@ public class RecursionQuestions {
         else return isSortedHelper(arr, index + 1);
     }
 
+    //returns true if array is sorted in ascending, false otherwise
     public static boolean isSorted(int arr[]) {
         return isSortedHelper(arr, 0);
+    }
+
+    //returns num raised to power pow, pow shall be non negative
+    public static long powerPositive(int num, int pow) {
+        if (pow == 0) return 1;
+        long halfPow = powerPositive(num, pow / 2);
+        if ((pow & 1) == 1) return num * halfPow * halfPow;
+        else return halfPow * halfPow;
+    }
+
+    //returns number of ways a floor of dimension 2*length can be covered by tiles of dimension 2*1
+    public static int tilingWays(int length){
+        if (length==0 || length==1) return 1;
+        return tilingWays(length-1)+tilingWays(length-2);
     }
 
 }
