f2fefcce7ada2a3d9f3f0e8c5e0421377c8da38a
JaTin <jigyashusaini7@gmail.com>
2023-12-06 17:40:24 +0530

    Adding quick sort method

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int arr[] = {1, 5, 7, 8, 3, 4, 2, 6, 8, 3, 2};
         System.out.println(Arrays.toString(arr));
-        DivideAndConquer.mergeSort(arr, 0, arr.length-1);
+        DivideAndConquer.quickSort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 }
--- src/divideAndConquer/DivideAndConquer.java
+++ src/divideAndConquer/DivideAndConquer.java
@@ -3,9 +3,11 @@ package divideAndConquer;
 import java.util.Arrays;
 
 public class DivideAndConquer {
+    //used to count the number of times a function is called
+    static int functionCallCount = 0;
 
+    //function to merge sort the given array
     public static void mergeSort(int arr[], int si, int ei) {
-
         if (si >= ei) return;
 
         int mid = si + (ei - si) / 2;
@@ -28,6 +30,30 @@ public class DivideAndConquer {
         while (j <= ei) temp[k++] = arr[j++];
 
         System.arraycopy(temp, 0, arr, si, k);
+    }
+
+    //function to quick sort the given array
+    public static void quickSort(int arr[], int si, int ei) {
+//        System.out.println("FC: " + ++functionCallCount);
+        if (si >= ei) return;
+
+        int pivot = arr[ei];
+        int i = -1;
+
+        for (int j = 0; j < ei; j++)
+            if (arr[j] <= pivot) {
+                int temp = arr[++i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        int temp = arr[++i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        quickSort(arr, si, i - 1);
+        quickSort(arr, i + 1, ei);
+
     }
 
 }
